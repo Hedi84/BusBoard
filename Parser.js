8bc9da7ce8551918d7fafc3a4f07492f3ce794c8
Damien Wheatley <damien.wheatley@harrisfederation.org.uk>
2018-11-05 10:56:04 +0000

    Parser Created

---
+++ Parser.js
@@ -0,0 +1,13 @@
+const got = require('got');
+const url = "https://api-radon.tfl.gov.uk/StopPoint/Mode/bus/Disruption/?app_id=f99ccd88&app_key=702440117b45a45f829df3026201c662"
+
+//Retrieves information from URL and stores it in an array.
+
+function parseURL(callback) {
+    var array = [];
+    got(url, { json: true }).then(response => {
+        array = response.body;
+        callback(array);
+    })
+}
+
