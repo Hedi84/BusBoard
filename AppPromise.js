acb9cf0c83e248439f671a3d8f5c08fec8baa4de
Hedi Van Meurs <alisoun@gmail.com>
2018-11-06 16:18:36 +0000

    promise

---
+++ AppPromise.js
@@ -0,0 +1,63 @@
+const readline = require('readline-sync');
+const importParse = require('./Parser.js');
+const importClass = require('./BusArrival.js');
+const moment = require('moment');
+
+//Filters information for user inputted Bus Code
+function filteringData(array) {
+    let retrievedData = [];
+    let numberOfBuses = Math.min(array.length,5);
+    for(i = 0; i <= numberOfBuses - 1; i++) {
+        retrievedData.push(new importClass.BusArrival(array[i].vehicleId,array[i].timeToStation,array[i].lineName,array[i].towards))
+    }
+    if (retrievedData.length > 0) {
+        retrievedData.sort(sortData);
+        retrievedData.forEach(function(element) {
+        console.log("Arriving " + moment().add(element.timeToStation,"seconds").fromNow() + " is bus " + element.lineName + " towards " + element.towards + " with vehicle id " + element.vehicleId )
+        })
+    } else {
+        console.log(`There was no information for that bus code :'(`);
+    }
+}
+
+//Provides the filtering criteria for the Parser and returns the filtered information into an array.
+function runProgram() {
+    var busCode = userInput();
+    parse().then(function(){
+      return filteringData(data)
+    });
+}
+
+let parse = function () {
+  return new Promise(function(resolve, reject) {
+    var url = createsURL(busCode);
+    var data = resolve(importParse.parseURL(url))
+    return data
+  })
+}
+
+function createsURL(input) {
+    const url = "https://api.tfl.gov.uk/StopPoint/" + input + "/Arrivals";
+    return url;
+}
+
+function userInput() {
+    console.log("Please enter the bus code:");
+    var userInput = readline.prompt();
+    return userInput;
+}
+
+function sortData(bus1,bus2) {
+    if (bus1.timeToStation < bus2.timeToStation) {
+        return -1;
+    } else if (bus1.timeToStation > bus2.timeToStation) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// runProgram();
+
+exports.filteringData = filteringData;
+exports.createsURL = createsURL
---
+++ ParserPromise.js
@@ -0,0 +1,19 @@
+const got = require('got');
+const readline = require('readline-sync');
+
+//Retrieves information from URL and stores it in an array.
+
+function parseURL(url) {
+  // console.log(url);
+    var array = [];
+    got(url, { json: true })
+        .then(response => {
+            array = response.body;
+            // console.log('Reached Callback');
+       })
+       return array
+       }
+
+
+
+exports.parseURL = parseURL;
